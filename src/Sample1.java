a3035d7365ace8a310bc1c3446f30642e3ef05d7
藤本侑真 <fujimoto@fujimotoyuushinnoMacBook-Air.local>
2022-03-27 20:22:41 +0900

    [Add]Sample4_file

--- src/Sample1.java
+++ src/Sample1.java
@@ -1,3 +1,4 @@
+//画面に文字を出力するコード
 class Sample1
  {
     public static void main(String[] args)
@@ -6,3 +7,24 @@ class Sample1
         System.out.println("Javaをはじめよう!");
     }
 }
+
+class Sample3
+{
+  public static void main(String[] args)
+  {
+      System.out.println('A');
+      System.out.println("ようこそJavaへ！");
+      System.out.println(123);
+  }  
+}
+
+class Sample4
+{
+    public static void main(String[] args)
+    {
+        System.out.println("円記号を表示します。:¥¥");
+        System.out.println("アポストロフィを表示します。:¥'");
+    }
+}
+
+
