a3826dfa29082748aa503b560b34ccb415d525f2
藤本侑真 <fujimoto@fujimotoyuushinnoMacBook-Air.local>
2022-03-30 21:30:58 +0900

    [Add]Sample6

--- src/Sample1.java
+++ src/Sample1.java
@@ -22,8 +22,28 @@ class Sample4
 {
     public static void main(String[] args)
     {
-        System.out.println("円記号を表示します。:¥¥");
-        System.out.println("アポストロフィを表示します。:¥'");
+        System.out.println("円記号を表示します。:\\");
+        System.out.println("アポストロフィを表示します。:\'");
+    }
+}
+
+class Sample5
+{
+    public static void main(String[] args)
+    {
+        System.out.println("8進数101の文字は\101です。");
+        System.out.println("16進数0061の文字は\u0061です。");
+    }
+}
+
+class Sample6
+{
+    public static void main(String[] args)
+    {
+        System.out.println("10進数の10は" + 10 + "です。");
+        System.out.println("8進数の10は" + 010 + "です。");
+        System.out.println("16進数の10は" + 0x10 + "です。");
+        System.out.println("16進数のFは" + 0xF + "です。");
     }
 }
 
