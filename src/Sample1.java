c71c7e26ad8fb093a2712622417f562598e7a255
藤本侑真 <fujimoto@fujimotoyuushinnoMacBook-Air.local>
2022-03-31 16:17:50 +0900

    [Add]Sample5_import

--- src/Sample1.java
+++ src/Sample1.java
@@ -51,12 +51,10 @@ class Sample7
 {
     public static void main(String[] args)
     {
-        int num;
-
-        num = 3;
-
+        int num = 3;
         System.out.println("変数numの値は" + num + "です。");
     }
 }
+
 
 
