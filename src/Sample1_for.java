4d8e357a527beb1a8758440d6fae8004f6d77b9c
藤本侑真 <fujimoto@fujimotoyuushinnoMacBook-Air.local>
2022-04-03 17:41:53 +0900

    [Add]Sample8_for

---
+++ src/Sample1_for.java
@@ -0,0 +1,13 @@
+class Sample1_for
+{
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=5; i++){
+            System.out.println("繰り返します。");
+        }
+
+        System.out.println("繰り返しが終わりますた。");
+
+    }
+    
+}
