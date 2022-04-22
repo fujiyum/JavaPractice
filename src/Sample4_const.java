2eb47825549bc8089bc67bc0aa979cd50c1027e0
藤本侑真 <fujimoto@fujimotoyuushinnoMacBook-Air.local>
2022-04-23 00:29:35 +0900

    [Update]Sample4_const

--- src/Sample4_const.java
+++ src/Sample4_const.java
@@ -5,7 +5,15 @@ class Car3
 
     public Car3()
     {
+        num = 0;
+        gas = 0.0;
         System.out.println("車を製作しました");
+    }
+    public void setCar(int n, double g)
+    {
+        num = n;
+        gas = g;
+        System.out.println("ナンバー" + num + "ガソリン量" + gas + "の車を製作しました。");
     }
     public void show()
     {
@@ -19,8 +27,12 @@ class Sample4_const
     public static void main(String[] args)
     {
         Car3 car1 = new Car3();
-
+        car1.setCar(1234, 20.5);
         car1.show();
+
+        Car3 car2 = new Car3();
+        car2.setCar(4567, 30.5);
+        car2.show();
     } 
     
 }
